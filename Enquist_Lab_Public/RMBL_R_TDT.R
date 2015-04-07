f703c9e995176ed8a8f563d557ca34c1cd47548a
Brian J. Enquist <benquist@email.arizona.edu>
2015-04-06 22:20:19 -0700

    additional edits and cleaning up

--- Enquist_Lab_Public/RMBL_R_TDT.R
+++ Enquist_Lab_Public/RMBL_R_TDT.R
@@ -1,7 +1,9 @@
-### Brian J. Enquist
-### analysis of RMBL flux and trait data for AECR 2015 paper
-#### used in Enquist et al. 2015
-##### RMBL NPP and flux data
+################################################
+###   Brian J. Enquist
+###   analysis of RMBL flux and trait data for AECR 2015 paper
+###   used in Enquist et al. 2015
+###   RMBL NPP and flux data
+################################################
 library(smatr)
 library(car)
 library(lmSupport)
@@ -16,7 +18,9 @@ library(AICcmodavg)
 ##### Note, except where noted, these values, including the trait data are already log10 transformed. 
 ####
 
-###RMBL NPP
+########################
+### RMBL NPP - Full model
+########################
 my.data <- read.csv(file="/Users/benquist/Desktop/ProdDivAll_transformed.csv",header=T)
 m1 <- lm(log10(NPP.BioOverTime.)~ CWM.SLA+ CWV.SLA + logBio, data=my.data)
 summary(m1) 
@@ -27,8 +31,25 @@ crPlots(m1)
 confint(m1)
 vif(m1) 
 
+#  Assess relationship between elevation and total biomass (there is no relationship)
+# theme_set(theme_gray(base_size = 20)) # 
+myplot <- ggplot(data=my.data, aes(x = Elevation, y = logBio))
+myplot <- myplot + geom_point(size = 4)
 
-###RMBL NEP
+### will tive a simple black and white graph 
+myplot + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+               panel.background = element_blank(), axis.line = element_line(colour = "black"))
+summary(myplot)
+myplot + geom_point()
+
+myplot <- ggplot(data=my.data, aes(x = CWM.SLA, y = logBio))
+summary(myplot)
+myplot + geom_point() 
+
+########################
+### RMBL NEP - Full model  
+########################
+
 m2<-lm(log10(posNEP)~ CWM.SLA+ CWV.SLA + logBio, data=my.data)
 summary(m2) #R2= 0.4877, p = 0.0001284
 AIC(m2)
@@ -38,8 +59,10 @@ crPlots(m2)
 confint(m2)
 vif(m2) 
 
+########################
+### RMBL NPP - Full model - RMBL NPP with site as a factor 
+########################
 
-### RMBL NPP with site as a factor
 my.data <- read.csv(file="/Users/benquist/Desktop/ProdDivAll_transformed.csv",header=T)
 m1 <- lm(log10(NPP.BioOverTime.)~ CWM.SLA+ CWV.SLA + logBio +as.factor(Site_name), data=my.data)
 summary(m1) #R2= 0.9567, p <0.0001
@@ -64,6 +87,8 @@ vif(m2)
 
 #############################################
 ###RMBL NEP with site as a factor no biomass
+############################################
+
 m2<-lm(log10(posNEP)~ CWM.SLA + CWV.SLA +as.factor(Site_name), data=my.data)
 summary(m2) 
 AIC(m2)
@@ -75,7 +100,10 @@ vif(m2)
 
 #############################
 ###RMBL NEP diversity########
+#############################
+
 m2<-lm(log10(posNEP)~ nbsp , data=my.data)
+# m2<-lm(log10(posNEP)~ nbsp +as.factor(Site_name), data=my.data) # using site as a factor
 summary(m2) 
 AIC(m2)
 modelEffectSizes(m2)
